3c9fdf7ac420d2a562177b673a9a513dc7f69b6a
mul <msarnojimenez+github@gmail.com>
2026-06-03 16:09:52 -0400

    Removed usage of test home page.
    Now the router goes directly to the auth page

--- src/shared/components/PagesBar.tsx
+++ src/shared/components/PagesBar.tsx
@@ -5,10 +5,6 @@ export default function PagesBar() {
 
   return (
     <nav className="w-full pages-bar bg-primary p-2 flex flex-row gap-2 text-sm font-bold text-accent">
-      <Link to="/"
-        className="block bg-secondary rounded px-2 py-1 hover:text-text">
-        Home
-      </Link>
       <Link to="/test"
         className="block bg-secondary rounded px-2 py-1 hover:text-text">
         Test
